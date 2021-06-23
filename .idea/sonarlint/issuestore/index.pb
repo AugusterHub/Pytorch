4e2c2ca16f60d8c1491c7ee7a00f919d6fb6377f
zhaipeng <zhaipeng@topscomm.com>
2021-06-23 20:16:13 +0800

    线性模型

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -2,3 +2,5 @@
 :
 
 pyvenv.cfg,c\8\c83673bacea4b327fb4a1a62dbd9e8a7efd41ab3
+F
+Include/liner_model.py,2\0\20a70f4c9c4f49586cbb424a4651b62420ccc8bd
--- Include/liner_model.py
+++ Include/liner_model.py
@@ -1,19 +1,18 @@
-'''  0 线性模型  '''
+""" 0 线性模型 """
 import numpy as np
 import matplotlib.pyplot as plt
 
 x_data = [1.0, 2.0, 3.0]
 y_data = [2.0, 4.0, 6.0]
 
-
 def forward(x):
+    # 前向传播
     return x * w
 
-
 def loss(x, y):
+    # 定义loss
     y_pred = forward(x)
     return (y_pred - y) * (y_pred - y)
-
 
 w_list = []
 mse_list = []
