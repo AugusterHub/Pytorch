d9444392d05763d93402c5e77f5dae8f8d3e9532
zhaipeng <zhaipeng@topscomm.com>
2021-07-18 20:10:08 +0800

    torch logisticRegression

---
+++ no.4_logistic_regression_torch.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import torch
+import torch.nn.functional as F
+
+""" 0 Prepare dataset"""
+x_data = torch.Tensor([[1.0], [2.0], [3.0]])
+y_data = torch.Tensor([[0], [0], [1]])
+
+""" 1 Design model using Class"""
+class LogisticRegressionModel(torch.nn.Module):
+    def __init__(self):
+        super(LogisticRegressionModel, self).__init__()
+        self.linear = torch.nn.Linear(1, 1)
+
+    def forward(self, x):
+        y_pred = F.sigmoid(self.linear(x))
+        return y_pred
+
+model = LogisticRegressionModel()
+""" 2 Construct loss and optimizer"""
+criterion = torch.nn.BCELoss(size_average=False)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+
+""" 3 Training cycle"""
+for epoch in range(1000):
+    y_pred = model(x_data)
+    loss = criterion(y_pred, y_data)
+    print(epoch, loss.item())
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+x_test = torch.Tensor([[4.0]])
+y_test = model(x_test)
+print('y_pred', y_test.data)
